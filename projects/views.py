89969119137cc2b580fc9f7a520a00ff7cd04119
yourname <yourname@gmail.com>
2025-03-19 09:00:28 -0800

    Added views for DIY projects, categories, and tags

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import DIYProject, Category, Tag
+from .serializers import DIYProjectSerializer, CategorySerializer, TagSerializer
 
-# Create your views here.
+# DIY Projects
+class DIYProjectListCreate(generics.ListCreateAPIView):
+    queryset = DIYProject.objects.all()
+    serializer_class = DIYProjectSerializer
+
+class DIYProjectDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DIYProject.objects.all()
+    serializer_class = DIYProjectSerializer
+
+# Categories
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# Tags
+class TagListCreate(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
